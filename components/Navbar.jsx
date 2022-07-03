60915fe9a6b3a68f616354b2ff665b87e0c1e090
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-07-03 22:23:52 +0300

    add

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Link from 'next/link';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div>
-      <div>
+    <div className='fixed left-0 top-0 w-full z-10 ease-in duration-300'>
+      <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 text-white'>
         <Link href='/'>
-          <h1>AYDev.</h1>
+          <h1 className='font-bold text-4xl cursor-pointer'>AYDev.</h1>
         </Link>
         <ul>
           <li>
@@ -22,6 +23,27 @@ const Navbar = () => {
             <Link href='/contact'>Contact</Link>
           </li>
         </ul>
+        {/* Mobile Button */}
+        <div>
+          <AiOutlineMenu size={20} />
+        </div>
+        {/* Mobile Menu */}
+        <div>
+          <ul>
+            <li>
+              <Link href='/'>Home</Link>
+            </li>
+            <li>
+              <Link href='/#gallery'>Gallery</Link>
+            </li>
+            <li>
+              <Link href='/portfolio'>My Work</Link>
+            </li>
+            <li>
+              <Link href='/contact'>Contact</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
