2bb134db79e85921695edadef9edca2406233de1
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-07-05 17:27:17 +0300

    add

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const Portfolio = () => {
   return (
-    <div className='max-w-[1240px] px-auto py-16 text-center'>
+    <div className='max-w-[1240px] px-auto py-16 text-center m-auto'>
       <h1 className='font-bold text-3xl p-4'>Travel Photos</h1>
       <div className='grid grid-rows-none md:grid-cols-5 p-4 gap-4'>
         <div className='w-full h-full col-span-2 md:col-span-3 row-span-2'>
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Hero from '../components/Hero'
 import Instagram from '../components/Instagram'
-import Navbar from '../components/Navbar'
 import Slider from '../components/Slider'
 import { SliderData } from '../components/SliderData'
 
@@ -19,8 +17,6 @@ export default function Home() {
       <Hero heading='AYDev Photography' message='I capture moments in nature and keep them alive.' />
       <Slider slides={SliderData} />
       <Instagram />
-
-
     </div >
   )
 }
