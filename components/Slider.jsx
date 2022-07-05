02ffa3e09da046201cf70f311daae61d4e00a391
Aleksandr Yehorov <64478695+Aleksegoist@users.noreply.github.com>
2022-07-05 12:48:06 +0300

    add

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -1,22 +1,57 @@
 import Image from 'next/image';
-import React from 'react';
+import React, { useState } from 'react';
 import { SliderData } from './SliderData';
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa';
 
 const Slider = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
   return (
-    <div id='gallery'>
-      <h1>Gallery of F1</h1>
-      <div>
+    <div id='gallery' className='max-w-[1240px] mx-auto'>
+      <h1 className='text-3xl font-bold text-center p-4'>Gallery of F1</h1>
+      <div className='relative flex justify-center p-4'>
         {SliderData.map((slide, index) => {
           return (
-            <Image
+            <div
               key={index}
-              src={slide.image}
-              alt='/'
-              width='1440'
-              height='600'
-              objectFit='cover'
-            />
+              className={
+                index === current
+                  ? 'opacity-[1] ease-in duration-1000'
+                  : 'opacity-0'
+              }
+            >
+              <FaArrowCircleLeft
+                onClick={prevSlide}
+                size={50}
+                className='absolute top-[50%] left-[30px] text-white/70 cursor-pointer select-none z-[2]'
+              />
+              {index === current && (
+                <Image
+                  src={slide.image}
+                  alt='/'
+                  width='1440'
+                  height='600'
+                  objectFit='cover'
+                />
+              )}
+              <FaArrowCircleRight
+                onClick={nextSlide}
+                size={50}
+                className='absolute top-[50%] right-[30px] text-white/70 cursor-pointer select-none z-[2]'
+              />
+            </div>
           );
         })}
       </div>
--- components/SliderData.js
+++ components/SliderData.js
@@ -13,6 +13,6 @@ export const SliderData = [
         image: 'https://images.unsplash.com/photo-1563556469065-2cb69c7a1900?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'
     },
     {
-        image: 'https://images.unsplash.com/photo-1552849397-7a2d7864a9b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
+        image: 'https://images.unsplash.com/photo-1552849397-7a2d7864a9b5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
     },
 ]
